64ed4665cbba9339489b279e2f942dabcc149f30
Nikolay Osipov <nick_osipov@mail.ru>
2023-04-14 18:01:20 +0300

    Created app cart. Add cart.py and context_processors. Add models Order and OrderItem in models.py. And integrated into the admin panel.

--- market/config/settings.py
+++ market/config/settings.py
@@ -59,7 +59,6 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'cart.context_processors.cart',
 ]
 
 if DEBUG:
